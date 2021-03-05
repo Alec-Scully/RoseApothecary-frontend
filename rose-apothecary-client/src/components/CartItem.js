02d8eb1c573d9383ecedb9e22aa8f48ea643b0af
Alec Scully <ScullyAlec@gmail.com>
2021-03-05 17:15:35 -0500

    All bugs fixed for patch/post/delete in cart

--- rose-apothecary-client/src/components/CartItem.js
+++ rose-apothecary-client/src/components/CartItem.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 
 class CartItem extends Component {
+    
+    test = () => {
+        let updateCartItem = this.props.cartItems.find(cartItem => cartItem.cart_id === this.props.user.cart.id && cartItem.item_id === this.props.item.id)
+        return <h4>Quantity: {updateCartItem.quantity}</h4>
+    }
 
     render() {
 
@@ -10,7 +15,8 @@ class CartItem extends Component {
                 <div >
                     <img src={this.props.item.image} alt={this.props.item.name + "image"} className="item-image"/>
                     <h2>{this.props.item.name}</h2>
-                    <h4>{this.props.item.cost}</h4>
+                    <h4>{"$" + this.props.item.cost}</h4>
+                    {this.test()}
                 </div>
                 <button onClick={() => this.props.removeFromCart(this.props.item)}>Remove from Cart</button>
             </div>
--- rose-apothecary-client/src/components/ItemCard.js
+++ rose-apothecary-client/src/components/ItemCard.js
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Redirect } from 'react-router-dom';
-import ItemDetail from './ItemDetail'
+// import ItemDetail from './ItemDetail'
 
 class ItemCard extends Component {
-
-    handleClick = () => {
-        {console.log("click")}
-        <Redirect to={`/products/${this.props.item.id}`}/>
-
-    }
 
     render() {
 
         return(
             <div className="card" >
-                <div onClick={() => this.handleClick()}>
+                <div onClick={() => <Redirect to={`/products/${this.props.item.id}`}/>}>
                     <img src={this.props.item.image} alt={this.props.item.name + "image"} className="item-image"/>
                     <h2>{this.props.item.name}</h2>
-                    <h4>{this.props.item.cost}</h4>
+                    <h4>{"$" + this.props.item.cost}</h4>
                 </div>
                 <button onClick={() => this.props.addToCart(this.props.item)}>Add to Cart</button>
             </div>
